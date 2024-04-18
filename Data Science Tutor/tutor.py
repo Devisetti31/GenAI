d8480ed82f2171b5cbb68d2b9d0938a7860739fb
DEVISETTI LAKSHMI NARASIMHARAO <147506187+Devisetti31@users.noreply.github.com>
2024-04-18 20:06:57 +0530

    Add files via upload

---
+++ Data Science Tutor/tutor.py
@@ -0,0 +1,31 @@
+import streamlit as st
+import google.generativeai as genai
+
+f = open("my_key.txt")
+key = f.read()
+
+genai.configure(api_key=key)
+
+st.title("🗣️Conversational AI Data Science Tutor 📚🤖🤵")
+
+ai=genai.GenerativeModel(model_name="gemini-1.5-pro-latest",
+    system_instruction="""You're an AI Teaching Assistant that provides answers to user queries related to data science topics. 
+                         If 'hai'or 'hi' in the users request, respond with politely. Otherwise, if the user's query is unrelated to 
+                         data science, respond with 'I'm sorry, I don't have information about that.' If the user's query is not a greeting 
+                         and is related to data science, provide an appropriate answer.""")
+if "chat_history" not in st.session_state:
+    st.session_state["chat_history"]=[]
+
+chat = ai.start_chat(history=st.session_state['chat_history'])
+for msg in chat.history:
+
+    st.chat_message(msg.role).write(msg.parts[0].text)
+
+user_prompt=st.chat_input()
+
+if user_prompt:
+    st.chat_message("user").write(user_prompt)
+    response=chat.send_message(user_prompt)
+    st.chat_message("ai").write(response.text)
+    #print(chat.history)
+    st.session_state["chat_history"]=chat.history
